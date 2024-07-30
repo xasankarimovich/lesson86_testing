d29c83db59a0cacd3bcc47f0c3bb642bb91aa7bc
Karimovich. <146013797+xasankarimovich@users.noreply.github.com>
2024-07-30 07:34:31 -0700

    obboooo

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:lesson86_testing/main.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:lesson86_testing/main.dart';
 
 void main() {
   group("Calculator TEST", () {
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_driver/flutter_driver.dart' as d;
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:lesson86_testing/main.dart';
 
 void main() {
